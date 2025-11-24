2606a2cfc8031113c36215669a4653e4a6085d14
leeeunji <ayozz6742@naver.com>
2025-11-24 21:47:34 +0900

    f

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/my-portfolio",
+  base:"/my-portfolio/",
   plugins: [
     react({
       babel: {
